89875f6a07372ec3de9594876f0c4c17303d98ed
Henil shah <145034285+Henilshah01@users.noreply.github.com>
2025-02-28 23:46:46 +0530

    Data Types

--- 1_Basics/2_dataTypes.js
+++ 1_Basics/2_dataTypes.js
@@ -4,4 +4,24 @@
 
 console.log(3 + 3) //COde readabilty should 
 
-console.log("Tech")
+console.log("Code")
+
+let name = "Tech"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// symbol => unique
+
+// object
+
+console.log(typeof "Tech");
+console.log(typeof null); // object 
+console.log(typeof undefined); //undefined
+
+
